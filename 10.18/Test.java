eba73f8e0746ed80304854b0f37b23716241fa72
MuqiuWhite <1219214097@qq.com>
2021-11-23 23:45:09 +0800

    java

---
+++ 10.18/Test.java
@@ -0,0 +1,37 @@
+import java.util.*
+public class Test{
+	public static void main(String args[]){
+
+		Random ran=new Random();
+		Scanner sc=new Scanner(System.in);
+		int g=ran.nextInt(1000);
+		whiletrue
+		int n=sc.nextInt();
+		while(sc.hasNexiInt())
+		{
+			int a=sc.nextInt();
+			 
+		}
+	}
+
+	public static void main1(String args[]){
+
+		Random ran = new Random();
+		Scanner sc = new Scanner(System in);
+		int guess = ran.nextInt(100);
+		while(true){
+
+			System.out.println("sr:");
+			int n = sc.nextInt();
+			if(n<guess)
+			{
+				System.out.println("xl");
+			}
+
+		}sc.hasNextDouble
+		sc.close;System.out.println(i+"*"+j+"="+i*j);
+		"i*j="+I*j
+	}i，=(int)Math.sqrt(n);
+
+
+}
