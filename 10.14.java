3bd49547503fdc151f31427aaf6fc5da7ed8bc4b
MuqiuWhite <1219214097@qq.com>
2021-12-02 23:00:34 +0800

    java

---
+++ 10.14.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class TEST{
+
+	public static void main(String[] args){
+		Scanner scan =new Scanner(System.in);
+
+		while(scan.hasNextInt()){
+
+			int num =scan.nextInt();
+			System.out.println(num);
+		}
+		
+
+
+	}
