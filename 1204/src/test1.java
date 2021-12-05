59b29cb3cd9d6441e796a615c6abfd79e41f7032
MuqiuWhite <1219214097@qq.com>
2021-12-05 20:58:54 +0800

    sql

---
+++ 1204/src/test1.java
@@ -0,0 +1,14 @@
+public class test1 {
+
+    public static void main(String[] args) {
+
+        int []a={1,2};
+        try {
+            System.out.println(a[2]);
+        }catch(ArrayIndexOutOfBoundsException e){
+            e.printStackTrace();
+            System.out.println("越界了大哥");
+        }
+        System.out.println("我显示出来啦");
+    }
+}
